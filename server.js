4ab969f8809be59515f214c050ad681d16e92d38
Kayalpiriya <sdilakshana1@gmail.com>
2026-03-28 12:37:41 +0530

    urt set

--- server.js
+++ server.js
@@ -19,7 +19,16 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:3000",
+    "https://exlabour-frontend.vercel.app"
+  ],
+  credentials: true
+}));
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
