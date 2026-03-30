cdb5e8efc4099b5c2ba709a30efdc1c6c6e998e4
Kayalpiriya <sdilakshana1@gmail.com>
2026-03-30 09:02:45 +0530

    add admin

--- src/models/User.js
+++ src/models/User.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    role: { type: String, enum: ['user', 'tasker'], required: true },
+    role: { type: String, enum: ['user', 'tasker', 'admin'], required: true },
     phone: { type: String },
     profileImage: { type: String },
     verificationStatus: {
